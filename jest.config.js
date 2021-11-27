910a8b36f5542406ee974c63d494105673f6513a
Eduard <ptushko@gmail.com>
2021-11-27 11:41:37 +0300

    Jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: {
-        '^.+\.tsx?$': 'ts-jest',
-    }
-};
+	preset: 'ts-jest',
+	globals: {
+		'ts-jest': {
+			tsconfig: 'tsconfig.jest.json',
+		},
+	},
+	collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+	moduleNameMapper: {
+		'^.+.module.(css|sass|scss)$': 'identity-obj-proxy',
+		'^.+.(css|sass|scss)$': '<rootDir>/test/__mocks__/styleMock.js',
+		'^.+.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/test/__mocks__/fileMock.js',
+	},
+	setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+	testEnvironment: 'jsdom',
+}
